78e774e5db12cf3342a41de6a7d770262dac6dab
soowankispassah <soowankispassah@gmail.com>
2025-01-03 20:00:54 +0530

    Update: Remove unused imports and update Projects component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import About from "@/components/About";
-import Blog from "@/components/Blog";
+//import Blog from "@/components/Blog";
 import Contact from "@/components/Contact";
-import Footer from "@/components/Footer";
+//import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
